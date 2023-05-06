f621b9d41b38892a1262eecaba272591d92678dd
Anthony Catullo <anthonycatullo@oatslam.cable.rcn.com>
2023-05-06 15:56:51 -0500

    added link to taskId page on taskList page

--- components/tasks/TaskItem.js
+++ components/tasks/TaskItem.js
@@ -1,11 +1,15 @@
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import Link from "next/link";
 
 export default function TaskItem({ task }) {
+  const exploreLink = `/dashboard/${task.id}`;
   return (
     <>
       <tbody key={task.id}>
         <tr className="border-b-2 text-center">
-          <td>{task.title}</td>
+          <Link href={exploreLink}>
+            <td>{task.title}</td>
+          </Link>
           <td>{task.difficulty}</td>
           <td>{task.completionDate}</td>
           <td className="text-center text-yellow-600">
