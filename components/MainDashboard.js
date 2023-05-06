2db351a1b51387518a028c8df8b120d33448e051
Anthony Catullo <anthonycatullo@oatslam.cable.rcn.com>
2023-05-06 01:41:55 -0500

    added updated styles to main dashboard

--- components/MainDashboard.js
+++ components/MainDashboard.js
@@ -7,27 +7,32 @@ export default function MainDashboard() {
 
   return (
     <>
-      {data.map((d) => (
-        <div className="border-2 my-4" key={d.id}>
-          <div>Completion Date: {d.completionDate}</div>
-          <table className="table-auto w-full">
-            <thead className="bg-slate-400">
-              <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Difficulty</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
+      <div className="border-2 my-4">
+        <table className="table-auto w-full">
+          <thead className="bg-[#6c63ff]">
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Difficulty</th>
+              <th>Completion Date</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          {data.map((d) => (
+            <tbody key={d.id}>
+              <tr className="border-b-2 text-center">
                 <td>{d.title}</td>
                 <td>{d.description}</td>
                 <td>{d.difficulty}</td>
+                <td>{d.completionDate}</td>
+                <td>EditIcon</td>
+                <td>DelIcon</td>
               </tr>
             </tbody>
-          </table>
-        </div>
-      ))}
+          ))}
+        </table>
+      </div>
     </>
   );
 }
